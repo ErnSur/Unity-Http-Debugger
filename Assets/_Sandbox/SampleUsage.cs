bb2c9d334fdee60b730ef66a230f0ec81b71ed0d
Ernest Suryś <ernest.surys@gmail.com>
2022-09-23 14:08:14 +0200

    updated sample scene

--- Assets/_Sandbox/SampleUsage.cs
+++ Assets/_Sandbox/SampleUsage.cs
@@ -1,42 +1,49 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
-using QuickEye.RequestWatcher;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 
-public class SampleUsage : MonoBehaviour
+namespace QuickEye.RequestWatcher.LoggingSample
 {
-    public HttpStatusCode code;
-
-    [ContextMenu("Log")]
-    public void Test()
+    public class SampleUsage : MonoBehaviour
     {
-        Debug.Log($"MES: {code}");
-    }
+        [SerializeField]
+        private float requestInterval;
 
-    [MenuItem("Test/SendSampleReq")]
-    public static void SendSampleRequest()
-    {
-        SendRequestAsync();
-    }
+        private float _remainingTimeToRequest;
 
-    private static async void SendRequestAsync()
-    {
-        var client = new HttpClient();
-        var request = new HttpRequestMessage
+        private void Update()
         {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri("http://dummy.restapiexample.com/api/v1/employee/1"),
-        };
-        using (var response = await client.SendAsync($"Employee", request))
+            _remainingTimeToRequest -= Time.deltaTime;
+            if (!(_remainingTimeToRequest <= 0))
+                return;
+            SendSampleRequest();
+            _remainingTimeToRequest = requestInterval;
+        }
+
+        [MenuItem("Debug Requests/Send Get Request")]
+        public static void SendSampleRequest()
         {
-            response.EnsureSuccessStatusCode();
-            var body = await response.Content.ReadAsStringAsync();
-            Debug.Log(body);
+            SendRequestAsync();
+        }
+        
+        private static async void SendRequestAsync(CancellationToken cancellationToken = default)
+        {
+            var client = new HttpClient(new LogMessageHandler(null,"Employee"));
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("http://dummy.restapiexample.com/api/v1/employee/1"),
+            };
+            
+            
+            using (var response = await client.SendAsync(request, cancellationToken))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+            }
         }
     }
 }
