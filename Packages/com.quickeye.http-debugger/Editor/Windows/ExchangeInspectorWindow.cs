d9f658b27ab2c155fe98c15c634d533acbc1c1eb
Ernest Suryś <ernest.surys@gmail.com>
2022-10-21 19:32:12 +0200

    Updated Exchange Window Fullscreen layout

--- Packages/com.quickeye.http-debugger/Editor/Windows/ExchangeInspectorWindow.cs
+++ Packages/com.quickeye.http-debugger/Editor/Windows/ExchangeInspectorWindow.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace QuickEye.WebTools.Editor
@@ -13,44 +14,52 @@ namespace QuickEye.WebTools.Editor
         }
 
         private ExchangeInspector _inspectorController;
-        private VisualElement _fullWindowRoot;
 
         public override VisualElement CreateInspectorGUI()
         {
             var root = GetRoot();
-            InitViewController();
+            InitViewController(root);
             return root;
         }
 
-        private void InitViewController()
+        private void InitViewController(VisualElement root)
         {
             var uxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
                 "Packages/com.quickeye.http-debugger/Editor/UIAssets/ExchangeInspector.uxml");
-            uxml.CloneTree(_fullWindowRoot);
-            _inspectorController = new ExchangeInspector(_fullWindowRoot);
-            var readOnly = serializedObject.FindProperty("isReadOnly").boolValue;
+            uxml.CloneTree(root);
+            _inspectorController = new ExchangeInspector(root);
             _inspectorController.Setup(serializedObject);
         }
 
         private VisualElement GetRoot()
         {
             var root = new VisualElement();
-            _fullWindowRoot = new VisualElement();
-            StretchVe(_fullWindowRoot);
+            root.name = "exchange-inspector";
+
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(
+                "Packages/com.quickeye.http-debugger/Editor/UIAssets/ExchangeInspector.style.uss");
 
             root.RegisterCallback<AttachToPanelEvent>(evt =>
             {
-                var fullParent = evt.destinationPanel.visualTree.Q(null, "unity-inspector-main-container");
-                fullParent.Add(_fullWindowRoot);
+                EnableClassForAllParents(root, "exchange-inspector-ancestor", true);
+                evt.destinationPanel.visualTree.styleSheets.Add(styleSheet);
             });
-            root.RegisterCallback<DetachFromPanelEvent>(evt => { _fullWindowRoot.RemoveFromHierarchy(); });
+            root.RegisterCallback<DetachFromPanelEvent>(evt =>
+            {
+                //change to destination panel to cause an exception
+                evt.originPanel?.visualTree?.styleSheets.Remove(styleSheet);
+            });
             return root;
         }
 
-        private static void StretchVe(VisualElement ve)
+        private static void EnableClassForAllParents(VisualElement root, string className, bool enabled)
         {
-            ve.style.position = Position.Absolute;
-            ve.style.bottom = ve.style.top = ve.style.left = ve.style.right = 0;
+            root = root.hierarchy.parent;
+            while (root != null)
+            {
+                root.EnableInClassList(className, enabled);
+                root = root.hierarchy.parent;
+            }
         }
 
         public override bool UseDefaultMargins() => false;
